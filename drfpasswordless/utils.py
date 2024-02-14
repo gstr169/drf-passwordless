bc866f4fa94bbc7e5542ed0638255aa8ab4e3a03
Karen Gevorgyan <karen.gevorgyan@argomedia.co>
2024-02-14 13:40:31 +0300

    fix utils

--- drfpasswordless/utils.py
+++ drfpasswordless/utils.py
@@ -1,5 +1,6 @@
 import logging
 import os
+from django.contrib.auth import get_user_model
 from django.core.exceptions import PermissionDenied
 from django.core.mail import send_mail
 from django.template import loader
